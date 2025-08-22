61fc310ae8e8ae11b62f51d681024f763c029db7
Hellwood1 <samburik123@gmail.com>
2025-08-22 22:26:52 +0200

    Upd. build fix.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 'use strict';
-import './js/offers.js'
-import './js/line.js'
-import './js/faq-accordion.js'
+
+import './js/offers.js';
+import './js/line.js';
+import './js/faq-accordion.js';
 import './js/modal.js';
 import './js/hero-swiper.js';
-import './js/offers.js'
+import './js/offers.js';
