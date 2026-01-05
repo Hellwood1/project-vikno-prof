2967c1a25accde96b9ea085ec301669f5d350a8f
Hellwood1 <samburik123@gmail.com>
2026-01-05 21:35:43 +0100

    fix-build

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
-    base: '/project-vikno-prof/',
+    base: './',
     build: {
       sourcemap: true,
       rollupOptions: {
